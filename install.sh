7488dbcc8a491488b077d49217b08e287fbf413c
nasa9084 <nasa9084@live.jp>
2016-02-03 01:21:38 +0900

    add except catch

--- install.sh
+++ install.sh
@@ -36,28 +36,48 @@ vecho()
 
 vecho "Download Redmine..."
 wget "http://www.redmine.org/releases/redmine-${redmine_ver}.tar.gz" -O "${wwwroot}/redmine.tar.gz"
+state=$?
 vecho "Redmine is downloaded."
 
-vecho "Expand downloaded tar.gz..."
-if [ $verbose_flg -eq 1 ]
+if [ $state -eq 0 ]
 then
-    tar zxvf "${wwwroot}/redmine.tar.gz" -C "${wwwroot}"
-else
-    tar zxf "${wwwroot}/redmine.tar.gz" -C "${wwwroot}"
+    vecho "Expand downloaded tar.gz..."
+    if [ $verbose_flg -eq 1 ]
+    then
+        tar zxvf "${wwwroot}/redmine.tar.gz" -C "${wwwroot}"
+        state=$?
+    else
+        tar zxf "${wwwroot}/redmine.tar.gz" -C "${wwwroot}"
+        state=$?
+    fi
+    vecho "Expand is successfully done."
 fi
-vecho "Expand is successfully done."
 
-vecho "Rename redmine directory..."
-mv ${wwwroot}/redmine-${redmine_ver}/ ${wwwroot}/${redminedir}/
-vecho "Renamed."
+if [ $state -eq 0 ]
+then
+    vecho "Rename redmine directory..."
+    mv ${wwwroot}/redmine-${redmine_ver}/ ${wwwroot}/${redminedir}/
+    state=$?
+    vecho "Renamed."
+fi
 
-vecho "Create database..."
-mysql -u root --password="${password}" -e "create database if not exists redmine_${uname};"
-mysql -u root --password="${password}" -e "grant all on redmine_${uname}.* to '${uname}'@'localhost' identified by '${upass}';"
-vecho "Database creation is done."
+if [ $state -eq 0 ]
+then
+    vecho "Create database..."
+    mysql -u root --password="${password}" -e "create database if not exists redmine_${uname};"
+    state=$?
+    if [ $state -eq 0 ]
+    then
+        mysql -u root --password="${password}" -e "grant all on redmine_${uname}.* to '${uname}'@'localhost' identified by '${upass}';"
+        state=$?
+    fi
+    vecho "Database creation is done."
+fi
 
-vecho "Config redmine database..."
-cat <<EOF > config/database.yml
+if [ $state -eq 0 ]
+then
+    vecho "Config redmine database..."
+    cat <<EOF > config/database.yml
 production:
   adapter: mysql2
   database: redmine_${uname}
@@ -66,28 +86,58 @@ production:
   password: "${upass}"
   encoding: utf8
 EOF
-vecho "Configuration is done."
+    state=$?
+    vecho "Configuration is done."
+fi
 
-vecho "Install bundler from gem..."
-gem install bundler
-vecho "Bundler is successfully installed."
+if [ $state -eq 0 ]
+then
+    vecho "Install bundler from gem..."
+    gem install bundler
+    state=$?
+    vecho "Bundler is successfully installed."
+fi
 
-vecho "Resolve dependency with bundler..."
-bundle install --without development test postgresql sqlite
-vecho "Dependency resolved."
+if [ $state -eq 0 ]
+then
+    vecho "Resolve dependency with bundler..."
+    bundle install --without development test postgresql sqlite
+    state=$?
+    vecho "Dependency resolved."
+fi
 
-vecho "Generate session-store secret key..."
-rake generate_secret_token
-vecho "Key is generated."
+if [ $state -eq 0 ]
+then
+    vecho "Generate session-store secret key..."
+    rake generate_secret_token
+    state=$?
+    vecho "Key is generated."
+fi
 
-vecho "Create table..."
-cd redmine
-RAILS_ENV=production rake db:migrate
-vecho "Table is created."
+if [ $state -eq 0 ]
+then
+    vecho "Create table..."
+    cd redmine
+    RAILS_ENV=production rake db:migrate
+    state=$?
+    vecho "Table is created."
+fi
 
-vecho "Sign in Default data..."
-RAILS_ENV=production rake redmine:load_default_data
-vecho "Default data is loaded."
+if [ $state -eq 0 ]
+then
+    vecho "Sign in Default data..."
+    RAILS_ENV=production rake redmine:load_default_data
+    state=$?
+    vecho "Default data is loaded."
+fi
 
-chown -R ${uname}:${uname} files log tmp public/plugin_assets
-chmod -R 755 files log tmp public/plugin_assets
+if [ $state -eq 0 ]
+then
+    chown -R ${uname}:${uname} files log tmp public/plugin_assets
+    state=$?
+    if [ $state -eq 0 ]
+    then
+        chmod -R 755 files log tmp public/plugin_assets
+        state=$?
+    fi
+fi
